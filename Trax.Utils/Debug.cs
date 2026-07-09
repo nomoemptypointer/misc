e8202b3bdc6248f056bcf096ac2744d547bfc578
nomo <51972322+nomoemptypointer@users.noreply.github.com>
2026-07-10 01:53:28 +0200

    Trax.Utils

---
+++ Trax.Utils/Debug.cs
@@ -0,0 +1,49 @@
+﻿namespace Trax.Utils
+{
+    public enum LogType
+    {
+        Error,
+        Info,
+        Warning,
+        Debug,
+        Custom,
+        Unknown
+    }
+
+    public class Debug
+    {
+        private static string logFilePath;
+
+        public static void Log(LogType type, string message, string codeContext = null)
+        {
+            string logMessage = $"[{DateTime.Now}] [{type.ToString().ToUpper()}]";
+            if (!string.IsNullOrEmpty(codeContext)) logMessage += $" [{codeContext}]";
+            logMessage += $" {message}";
+            Console.WriteLine(logMessage);
+            WriteLogToFile(logMessage);
+        }
+
+        private static void WriteLogToFile(string logMessage)
+        {
+            string logsDirectory = "logs";
+            if (!Directory.Exists(logsDirectory))
+            {
+                Directory.CreateDirectory(logsDirectory);
+            }
+            string dateStamp = DateTime.Now.ToString("yyyyMMdd");
+            logFilePath = Path.Combine(logsDirectory, $"{dateStamp}.txt");
+
+            try
+            {
+                using (StreamWriter writer = File.AppendText(logFilePath))
+                {
+                    writer.WriteLine(logMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing to log file: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Trax.Utils/EnvironmentTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trax.Utils
+{
+    public class EnvironmentTable
+    {
+        private const string envName = "BOT_TOKEN";
+        public static string GetToken()
+        {
+            return Environment.GetEnvironmentVariable(envName) ?? throw new Exception($"Cannot login into the bot because there is no such field \"{envName}\" or it is empty.");
+        }
+    }
+}
